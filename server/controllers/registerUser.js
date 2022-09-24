49cd2f4c8b9befada24e4a3076994dfd9de66cec
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-09-24 15:44:58 +0200

    added the log in controller

--- server/controllers/registerUser.js
+++ server/controllers/registerUser.js
@@ -13,8 +13,29 @@ const registerUser = async (req, res) => {
     .json({ user: { username: UserDB.username, name: UserDB.name }, token });
 };
 
-const login = (req, res) => {
-  res.json({ msg: "User Logged In" });
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Please enter a valid email or password");
+  }
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json("The email you've entered isn't registered");
+  }
+  const correctPassword = await user.comparePassword(password);
+  if (!correctPassword) {
+    return res.status(404).json("Wrong Password");
+  }
+  const token = user.createJWT();
+  res.json({
+    user: {
+      name: user.name,
+      email: user.email,
+      userName: user.username,
+      isAdmin: user.isAdmin,
+    },
+    token,
+  });
 };
 
 module.exports = { registerUser, login };
