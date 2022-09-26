9539a4030882c4295f8eaca8c52271d894f43341
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-09-26 20:18:21 +0200

    finished making the styles and state management of the hero section

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,13 +1,16 @@
 import React from "react";
 import hero from "../assets/hero (2).jpg";
+import searchIcon from "../assets/loupe.png";
 
 function Hero() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
     <section className="hero relative h-screen overflow-hidden flex justify-center items-center">
       <img
         src={hero}
         alt="hero icon "
-        className="absolute bottom-0 w-screen -z-10"
+        className="absolute w-screen h-screen -z-10"
       />
       <div className="info translate-x-1/2  flex flex-col gap-y-5">
         <h2 className="text-5xl text-light-blue font-bold ">
@@ -18,6 +21,17 @@ function Hero() {
           egyptian company that provides high quality furniture for resonable
           prices
         </p>
+        <div className="flex w-fit border-2 border-light-red shadow-md ">
+          <input
+            type="text"
+            autoFocus={true}
+            className="text-3xl bg-white h-10 indent-1 focus:outline-none py-1 px-2"
+            onInput={(e) => setSearchValue(e.target.value)}
+          />
+          <button className="bg-white">
+            <img src={searchIcon} alt="Search Icon" className="h-10 p-1" />
+          </button>
+        </div>
       </div>
     </section>
   );
