def43510a59d289e8c6e0d589e226cb1fec3c8c9
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-10-05 01:36:44 +0200

    change in the server files

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ function App() {
       {cartOverlay && (
         <Cart handleOverLay={setCartOverlay} cart={cart} handleCart={setCart} />
       )}
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <UserContext.Provider
           value={{
             user,
